695afc8f06a2e8a4eacdd98ead7442cabd6c34c7
Alireza <alirezasaadat056@gmail.com>
2024-07-05 10:45:00 -0700

    enemy shooting

--- mian.py
+++ mian.py
@@ -1,9 +1,12 @@
 import pygame
 from sys import exit
-
+import random
 projectile_lst = []
 
 enemy_lst = []
+enemy2_lst = []
+
+enemy_shoot_lst = []
 
 WIDTH , HEIGHT = 1200 , 700
 
@@ -31,23 +34,26 @@ class Player:
         
     def draw(self):
         if self.goingright and self.x + 25 < WIDTH:
-            self.x += 3
+            self.x += 4
         if self.goingleft and self.x - 25 > 0:
-            self.x -= 3
+            self.x -= 4
             
         self.player_surface_rect = self.player_surface.get_rect(center = (self.x , self.y))
         screen.blit(self.player_surface , self.player_surface_rect)
 
 class Projectile:
-    def __init__(self , x , y):
+    def __init__(self , x , y , color):
         self.x = x
         self.y = y
-        
+        self.color = color
         self.projectile_surface = pygame.Surface( (5 , 10) )
-        self.projectile_surface.fill("white")
+        self.projectile_surface.fill(color)
     
-    def draw(self):
-        self.y -= 6
+    def draw(self , side):
+        if side == "firend":
+            self.y -= 7
+        else:
+            self.y += 3
         
         self.projectile_surface_rect = self.projectile_surface.get_rect(center = (self.x , self.y))
         screen.blit(self.projectile_surface , self.projectile_surface_rect)
@@ -56,19 +62,25 @@ class Enemy:
     def __init__(self , x , y , color):
         self.x = x
         self.y = y
+        self.color = color
         self.enemy_surface = pygame.Surface( (40 , 40) )
         self.enemy_surface.fill(color)
-    
+
+    def shoot(self):
+        enemy_shoot_lst.append(Projectile(self.x , self.y , self.color))
+
     def draw(self):
+        
         self.enemy_surface_rect = self.enemy_surface.get_rect(center = (self.x , self.y))
         screen.blit(self.enemy_surface , self.enemy_surface_rect)
 
 def create_enemy():
-    unitx = WIDTH / 11
+    unitx = WIDTH / (len(map1[0])+1)
     unity = 70
     for i in range(len(map1)):
         for j in range(len(map1[i])):
             enemy_lst.append(Enemy(unitx * (j + 1) - 40 , unity * (-1) * (i + 1) - 40 , color_lst[i]))
+            enemy2_lst.append(Enemy(unitx * (j + 1) - 40 , unity * (-1) * (i + 1) - 40 , color_lst[i]))
         
 create_enemy()
 dis = enemy_lst[0].x
@@ -79,25 +91,40 @@ def enemy_move(counter):
         for enemy in enemy_lst:
             enemy.y += 2
     else:
-        if side == "left" and enemy_lst[index].x <= WIDTH - dis:
+        if side == "left" and enemy2_lst[index].x <= WIDTH - dis:
             for enemy in enemy_lst:
                 enemy.x += 1
-            if enemy_lst[index].x >= WIDTH - dis:
+            for enemy in enemy2_lst:
+                enemy.x += 1
+            if enemy2_lst[index].x >= WIDTH - dis:
                 side = "down"
         elif side == "down":
             for enemy in enemy_lst:
                 enemy.y += 30
-            if enemy_lst[0].x <= 0 + dis+5:
+            for enemy in enemy2_lst:
+                enemy.y += 30
+            if enemy2_lst[0].x <= 0 + dis+5:
                 side = "left"
             else:
                 side = "right"
-        elif side == "right" and enemy_lst[0].x >= 0 + dis:
+        elif side == "right" and enemy2_lst[0].x >= 0 + dis:
             for enemy in enemy_lst:
                 enemy.x -= 1
-            if enemy_lst[0].x <= 0 + dis:
+            for enemy in enemy2_lst:
+                enemy.x -= 1
+            if enemy2_lst[0].x <= 0 + dis:
                 side = "down"
-                
-        
+        if counter % 120 == 0:
+            random.choice(enemy_lst).shoot()
+            
+            
+def check():
+    for pro in projectile_lst:
+        for enemy in enemy_lst:
+            if pro.projectile_surface.get_rect(center = (pro.x , pro.y)).colliderect(enemy.enemy_surface_rect):
+                projectile_lst.remove(pro)
+                enemy_lst.remove(enemy)
+                break
 
 def draw():
     screen.fill("black")
@@ -106,7 +133,10 @@ def draw():
     for projectile in projectile_lst:
         if projectile.y < 0:
             projectile_lst.remove(projectile)
-        projectile.draw()
+        projectile.draw("firend")
+    
+    for shoot in enemy_shoot_lst:
+        shoot.draw("enemy")
     
     for enemy in enemy_lst:
         enemy.draw()
@@ -132,13 +162,13 @@ while True:
             if event.key == pygame.K_RIGHT:
                 player.goingright = True
             if event.key == pygame.K_SPACE:
-                projectile_lst.append(Projectile(player.x , player.y))
+                projectile_lst.append(Projectile(player.x , player.y , "white"))
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 player.goingleft = False
             if event.key == pygame.K_RIGHT:
                 player.goingright = False
-                
+    check()
     enemy_move(counter)
     draw()
     counter += 1
